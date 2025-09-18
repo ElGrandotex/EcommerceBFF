d1001161b10b4a4744b6d38b6b66a633f558e412
ElGrandotex <jose.verdezoto.moran@gmail.com>
2025-09-18 13:46:39 -0500

    add routing configuration for dashboard and table components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./core/page/dashboard/dashboard').then(m => m.Dashboard),
+    children: [
+      {
+        path: 'table',
+        loadComponent: () => import('./core/components/table/table').then(m => m.Table)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
